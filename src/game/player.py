ffd1a4ef907e19a77a81431fb04f95c403cb33c7
fogsaturate <brangaming95@gmail.com>
2025-11-22 08:06:57 -0500

    parallaxxxx

--- src/game/player.py
+++ src/game/player.py
@@ -16,7 +16,7 @@ class PlayerManager:
         self.camera_pivot = self.camera.position
         self.parallax = 1.0
 
-        self.sensitivity = 0.05
+        self.sensitivity = 0.09
 
         self.spin: bool = False
 
@@ -34,19 +34,29 @@ class PlayerManager:
 
     # Input Update
     def update(self):
-        mouse_movement_x = rl.get_mouse_delta().x * self.sensitivity
-        mouse_movement_y = rl.get_mouse_delta().y * self.sensitivity
-        rl.update_camera_pro(self.camera, [0.0,0.0,0.0], [mouse_movement_x, mouse_movement_y, 0.0], 0.0)
-
-        cam_pos = self.camera.position
+        mouse_delta = rl.Vector2(rl.get_mouse_delta().x, rl.get_mouse_delta().y)
+        motion = rl.vector2_scale(mouse_delta, self.sensitivity)
 
         # Simulation of Godot's Basis.Z
         look = rl.vector3_subtract(self.camera.target, self.camera.position)
-        
-        z_correction = rl.vector2_scale(rl.Vector2(look.x, look.y), abs(cam_pos.z / look.z))
+        look_distance = -self.camera.position.z / look.z # distance between the cursor and the camera
+        look_normalized = rl.vector3_normalize(look)
 
-        self.cursor_position = z_correction
+        # Parallax Calculation (Direct translation of kermeet's math, thank you)
+        pivot = rl.Vector3(0.0, 0.0, 7.0)
+        cursor_vector3 = rl.Vector3(self.cursor_position.x, self.cursor_position.y, 0)
+        parallax_correction = rl.vector3_scale(cursor_vector3, self.parallax / 4.0)
+        z_correction = rl.vector3_scale(look_normalized, 0.5) # Basis.Z / 2f
+        self.camera.position = rl.vector3_add(pivot, rl.vector3_add(parallax_correction, z_correction))
+        # End of Parallax Calculation
 
+        # Input/Cursor Movement Logic
+        rl.update_camera_pro(self.camera, [0.0,0.0,0.0], [motion.x, motion.y, 0.0], 0.0)
+
+        self.cursor_position = rl.Vector2(
+            self.camera.position.x + look.x * look_distance,
+            self.camera.position.y + look.y * look_distance
+        )
 
         
     # Draw Update (for cursor textures n such)
