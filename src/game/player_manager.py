c253912a5f7a09e4018363cf6e69c7d7c1244da4
fogsaturate <brangaming95@gmail.com>
2025-11-25 06:45:04 -0500

    cursor drift done

--- src/game/player_manager.py
+++ src/game/player_manager.py
@@ -65,15 +65,31 @@ class PlayerManager:
 
         # End of Parallax Calculation
 
+        if player_settings.cursor_drift:
+            self.cursor_position = self.clamped_cursor_position
+
+
 
     # Draw Update (for cursor textures n such)
+
     def draw(self):
         rl.draw_model(self.cursor_plane, [self.clamped_cursor_position.x, self.clamped_cursor_position.y, 0.0], 1.0, rl.WHITE)
 
-        print(player_settings.cursor_drift)
-        if not player_settings.cursor_drift and self.max_abs(self.cursor_position.x, self.cursor_position.y) > 2.7375:
-            rl.draw_model(self.cursor_plane, [self.cursor_position.x, self.cursor_position.y, 0.0], 1.0, rl.RED)
-    
+        abs_distance = self.max_abs(self.cursor_position.x, self.cursor_position.y)
+        if not player_settings.cursor_drift and abs_distance > 2.7375:
+
+            if player_settings.buffer_cursor_fade_distance != 0.0:
+                ghost_cursor_color = rl.Color(255,255,255,0)
+                ghost_cursor_color.a = self.range_normalized(abs_distance, 2.7375, 2.7375 + abs(player_settings.buffer_cursor_fade_distance))
+
+                rl.draw_model(self.cursor_plane, [self.cursor_position.x, self.cursor_position.y, 0.0], 1.0, ghost_cursor_color)
+
+            else:
+
+                rl.draw_model(self.cursor_plane, [self.cursor_position.x, self.cursor_position.y, 0.0], 1.0, rl.WHITE)
+
+    # Input Handling
+
     def update_spin(self, motion: rl.Vector2):
 
         rl.update_camera_pro(self.camera, [0,0,0], [motion.x * 7.5, -motion.y * 7.5, 0.0], 0.0)
@@ -89,8 +105,14 @@ class PlayerManager:
         self.camera.target = rl.Vector3(self.camera.position.x, self.camera.position.y, 0)
         self.cursor_position = rl.vector2_add(self.cursor_position, motion)
 
+    # Math Functions
+
     def clamp(self, v, mn, mx):
         return max(min(v, mx), mn)
     
     def max_abs(self, v1, v2):
         return max(abs(v1), abs(v2))
+
+    def range_normalized(self, x, mn, mx):
+        x = self.clamp(x, mn, mx)
+        return int((x - mn) / (mx - mn) * 255)
