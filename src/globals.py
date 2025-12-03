3463fda5e7d4f59dc84b5b08661e4abc439bd3fd
fogsaturate <brangaming95@gmail.com>
2025-12-02 19:15:14 -0500

    ill continue this later

--- src/globals.py
+++ src/globals.py
@@ -13,3 +13,45 @@ settingsmgr = SettingsManager()
 settingsmgr.read_settings()
 
 settings = settingsmgr.settings
+
+# Module Stuff head
+
+def clamp(v, mn, mx):
+    return max(min(v, mx), mn)
+
+def linear_step(mn, mx, step):
+    return clamp((step - mn) / (mx - mn), 0, 1)
+
+class Node:
+    def __init__(self, data: any):
+        self.data = data
+        self.next = None
+        self.previous = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head: Node | None = None # Start of List
+        self.tail: Node | None = None # End of List
+        
+    def add_first(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+
+        if self.head:
+            self.head.previous = new_node
+        else:
+            self.tail = new_node
+        
+        self.head = new_node
+    
+    def add_last(self, data):
+        new_node = Node(data)
+
+        # Make a new list
+        if self.tail == None:
+            self.head = new_node
+            self.tail = new_node
+
+        self.tail.next = new_node
+        new_node.previous = self.tail
+        self.tail = new_node
